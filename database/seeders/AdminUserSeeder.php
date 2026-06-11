088ea0647eb84bb9182b6d525938e77253f0e4a9
angelito <tubigangelito981@gmail.com>
2026-06-11 15:12:07 +0800

    Seed Railway admin user

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => env('ADMIN_EMAIL', 'admin@sabc.local')],
+            [
+                'name' => 'SABC Admin',
+                'password' => env('ADMIN_PASSWORD', 'change-me'),
+                'role' => 'admin',
+            ],
+        );
+    }
+}
